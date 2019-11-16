5701fc8edeb0420d2d34b7d0c88c2688c7508f62
Ram Nad <ramnad1999@gmail.com>
2019-11-16 19:52:37 +0530

    indentaion fixed

--- users/management/commands/create_default_groups.py
+++ users/management/commands/create_default_groups.py
@@ -10,16 +10,17 @@ Director isn't added to any group at the moment. Permissions are defined in Dire
 Usage: manage.py create_groups
 """
 
-import logging
 
+import logging
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import Permission
+GROUPS = ['admins', 'professors', 'staff', ]
+# If more models are create they need to be added in this list
+MODELS = ['professor', 'director', 'staff', ]
+PERMISSIONS = ['view', 'add', 'delete', 'change', ]
+PERMISSIONS_LIMIT = ['view', ]
 
-GROUPS = ['admins', 'professors', 'staff',]
-MODELS = ['professor', 'director', 'staff',] #If more models are create they need to be added in this list
-PERMISSIONS = ['view', 'add', 'delete', 'change',]
-PERMISSIONS_LIMIT = ['view',]
 
 class Command(BaseCommand):
     help = 'Creates default permission groups for admins, professors and staff'
@@ -28,29 +29,31 @@ class Command(BaseCommand):
         for group in GROUPS:
             new_group, created = Group.objects.get_or_create(name=group)
             for model in MODELS:
-            	if group not 'admins':
-            		for permission in PERMISSIONS_LIMIT:
-	                    name = 'Can {} {}'.format(permission, model)
-	                    print("Creating {}".format(name))
+                if group != 'admins':
+                    for permission in PERMISSIONS_LIMIT:
+                        name = 'Can {} {}'.format(permission, model)
+                        print("Creating {}".format(name))
 
-	                    try:
-	                        model_add_perm = Permission.objects.get(name=name)
-	                    except Permission.DoesNotExist:
-	                        logging.warning("Permission not found with name '{}'.".format(name))
-	                        continue
+                        try:
+                            model_add_perm = Permission.objects.get(name=name)
+                        except Permission.DoesNotExist:
+                            logging.warning(
+                                "Permission not found with name '{}'.".format(name))
+                            continue
 
-	                    new_group.permissions.add(model_add_perm)
+                        new_group.permissions.add(model_add_perm)
                 else:
-                	for permission in PERMISSIONS:
-	                    name = 'Can {} {}'.format(permission, model)
-	                    print("Creating {}".format(name))
+                    for permission in PERMISSIONS:
+                        name = 'Can {} {}'.format(permission, model)
+                        print("Creating {}".format(name))
 
-	                    try:
-	                        model_add_perm = Permission.objects.get(name=name)
-	                    except Permission.DoesNotExist:
-	                        logging.warning("Permission not found with name '{}'.".format(name))
-	                        continue
+                        try:
+                            model_add_perm = Permission.objects.get(name=name)
+                        except Permission.DoesNotExist:
+                            logging.warning(
+                                "Permission not found with name '{}'.".format(name))
+                            continue
 
-	                    new_group.permissions.add(model_add_perm)
+                        new_group.permissions.add(model_add_perm)
 
         print("Created default group and permissions.")
